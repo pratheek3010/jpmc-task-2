21a5b5d866a450d0f25f3fae894d191d16eb478e
pratheek3010 <pratheekraviprakash@gmail.com>
2022-12-27 19:14:34 +0530

    completed task-2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  displayGraph: boolean,
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      displayGraph: false,
     };
   }
 
@@ -29,18 +31,31 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    if(this.state.displayGraph){
+      return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    let time = 0
+
+    //process is intiated every 0.1 sec 
+    const interval = setInterval(()=> {
+      //updating the values that client recieves after every 1 sec
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ 
+          data: serverResponds,
+          displayGraph: true, 
+        });
+      });
+      time = time + 1
+      if(time > 1000){
+        clearInterval(interval)
+      }
+    }, 100)
   }
 
   /**
